7248b32a9f5c6b0798fe584b09fdb00fba701098
SUP'Internet 11 <supinternet11@macbook-air-de-supinternet-3.local>
2019-06-18 14:13:05 +0200

    Initial Commit

---
+++ TicTacToc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TicTacToc
+//
+//  Created by SUP'Internet 11 on 18/06/2019.
+//  Copyright © 2019 SUP'Internet 11. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
